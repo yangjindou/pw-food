16d079b000ad2c5f434ad12aa09d81aa0c4c81e7
yangjd <1012641301@qq.com>
2022-03-29 16:48:36 +0800

    框架整理

--- web/src/main.js
+++ web/src/main.js
@@ -5,13 +5,6 @@ import store from "./store";
 import "./core/antdv.use";
 import "./core/axios.config";
 
-import ECharts from 'vue-echarts';
-Vue.component('v-chart', ECharts);
-
-import $ from 'jquery';
-Vue.prototype.$ = $;
-
-
 Vue.config.productionTip = false;
 new Vue({
   router,
