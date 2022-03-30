4306fe19a0f6146253ed6c89c324cd3f96d95ab1
yangjd <1012641301@qq.com>
2022-03-30 14:31:54 +0800

    异常统一处理

--- server/src/main/java/com/dou/server/exception/ControllerExceptionHandler.java
+++ server/src/main/java/com/dou/server/exception/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.dou.server.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +20,7 @@ import java.util.Map;
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
+    // token等鉴权异常
     @ExceptionHandler(AuthException.class)
     public ResponseEntity<?> authExceptionHandler(AuthException e) {
         Map<String,Object> map = new HashMap<>();
@@ -26,6 +28,7 @@ public class ControllerExceptionHandler {
         return new ResponseEntity<>(map, HttpStatus.UNAUTHORIZED);
     }
 
+    // 业务逻辑异常
     @ExceptionHandler(LogicException.class)
     public ResponseEntity<?> logicExceptionHandler(LogicException e) {
         Map<String,Object> map = new HashMap<>();
@@ -33,9 +36,18 @@ public class ControllerExceptionHandler {
         return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
     }
 
+    // redis连接异常
+    @ExceptionHandler(RedisConnectionFailureException.class)
+    public ResponseEntity<?> redisConnectionFailureExceptionHandler(RedisConnectionFailureException e) {
+        return new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+
+    // 其他异常处理
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> exceptionHandler(Exception e) {
+        Map<String,Object> map = new HashMap<>();
+        map.put("message", e.getMessage());
         e.printStackTrace();
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(map, HttpStatus.BAD_REQUEST);
     }
 }
