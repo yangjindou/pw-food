aec833db7da73ef443d5474e8cedc8176d0a9b4a
yangjd <1012641301@qq.com>
2022-03-31 13:20:50 +0800

    事物处理

--- server/src/main/java/com/dou/server/service/UserService.java
+++ server/src/main/java/com/dou/server/service/UserService.java
@@ -4,6 +4,7 @@ import com.dou.server.exception.LogicException;
 import com.dou.server.model.Pagination;
 import com.dou.server.model.User;
 import com.github.pagehelper.PageInfo;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,12 +17,16 @@ public interface UserService {
 
     User verifyUser(User temp) throws Exception;
 
+    @Transactional(rollbackFor = Exception.class)
     void add(User temp) throws Exception;
 
+    @Transactional(rollbackFor = Exception.class)
     void passwordModify(String oldPwd, String newPwd) throws Exception;
 
+    @Transactional(rollbackFor = Exception.class)
     void update(User temp) throws Exception;
 
+    @Transactional(rollbackFor = Exception.class)
     void delete(List<?> ids) throws LogicException;
 
     PageInfo<User> getPage(Pagination pagination, User temp);
