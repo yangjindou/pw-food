e25579b653d243fc5e14637155f188049dcbdfed
yangjd <1012641301@qq.com>
2022-04-02 18:01:29 +0800

    数据库备份

--- server/src/main/resources/devdb.sql
+++ server/src/main/resources/devdb.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100419
  File Encoding         : 65001
 
- Date: 01/04/2022 18:04:40
+ Date: 02/04/2022 17:34:32
 */
 
 SET NAMES utf8mb4;
@@ -28,14 +28,37 @@ CREATE TABLE `sys_dict`  (
   `update_date` datetime NULL DEFAULT NULL,
   `update_user` int NULL DEFAULT NULL,
   `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '名称',
-  `key` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT 'key',
+  `sign` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '标识',
   `remark` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '备注',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `login_name`(`name`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 72 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 74 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_dict
+-- ----------------------------
+INSERT INTO `sys_dict` VALUES (73, '2022-04-02 09:02:01', 1, '2022-04-02 09:02:06', 1, '1', '2', '3');
+
+-- ----------------------------
+-- Table structure for sys_dict_data
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_dict_data`;
+CREATE TABLE `sys_dict_data`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `create_date` datetime NOT NULL,
+  `create_user` int NULL DEFAULT NULL,
+  `update_date` datetime NULL DEFAULT NULL,
+  `update_user` int NULL DEFAULT NULL,
+  `pid` int NULL DEFAULT NULL COMMENT 'dict_id',
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '名称',
+  `value` varchar(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '值',
+  `sort` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '排序',
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `login_name`(`name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 74 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sys_dict_data
 -- ----------------------------
 
 -- ----------------------------
@@ -55,12 +78,12 @@ CREATE TABLE `sys_user`  (
   `role` varchar(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '角色',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `login_name`(`login_name`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 72 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 80 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
 INSERT INTO `sys_user` VALUES (1, '2022-03-30 11:35:02', NULL, '2022-04-01 08:47:38', 1, 'a', '系统管理员333', 'ca2@!1cf@aa!23b5b#6f4@adc0560adg#6@2d!6664c5@3d4c35@df413d6*223a', 'M07KjwZBHfVWO93I', '管理员');
-INSERT INTO `sys_user` VALUES (71, '2022-04-01 08:30:19', 1, NULL, NULL, 'ccz', 'z', 'bf!4d*!c6!!g20113310bd@Fc@02d20fa3a#3F6!b04a6c4F*3*2@1#b@g@d5a2!', '2MZPwz2DehNBzVgt', '监管仓管理员');
+INSERT INTO `sys_user` VALUES (79, '2022-04-02 08:56:57', 1, NULL, NULL, '测试', '1', 'c40a!b#cdac*0d*206cgbfaaab#6da1c64c15f*16*!f0g5#c13c0d6gd33d554b', 'at5rJapu0Phouq7a', '监管仓管理员');
 
 SET FOREIGN_KEY_CHECKS = 1;
