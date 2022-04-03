ec935cb81a10bcb187e2937b281066bea82e6540
yangjd <1012641301@qq.com>
2022-04-03 14:07:04 +0800

    修正

--- server/src/main/java/com/dou/server/service/impl/DictServiceImpl.java
+++ server/src/main/java/com/dou/server/service/impl/DictServiceImpl.java
@@ -1,12 +1,9 @@
 package com.dou.server.service.impl;
 
 import com.dou.server.mapper.DictMapper;
-import com.dou.server.mapper.UserMapper;
 import com.dou.server.model.Dict;
 import com.dou.server.model.Pagination;
-import com.dou.server.model.User;
 import com.dou.server.service.DictService;
-import com.dou.server.service.UserService;
 import com.dou.server.sql.ICriteria;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -24,13 +21,11 @@ import java.util.List;
 public class DictServiceImpl extends BaseServiceImpl<Dict> implements DictService {
 
     private final DictMapper dictMapper;
-    private final UserMapper userMapper;
-    private final UserService userService;
 
     @Override
     public PageInfo<Dict> getPage(Pagination pagination, Dict temp) {
         PageHelper.startPage(pagination.getPageNum(), pagination.getPageSize());
-        Example example = new Example(User.class);
+        Example example = new Example(Dict.class);
         ICriteria criteria = new ICriteria(example);
         criteria.andLike("name", temp.getName())
                 .andLike("sign", temp.getSign());
